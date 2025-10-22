37348e808d6219682b51c2e7962052779b32526d
mariaclr24 <160483532+mariaclr24@users.noreply.github.com>
2025-10-22 12:00:00 +0100

    ER em C#

---
+++ ER/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string matricula = "";
+        bool matriculaOk = false;
+
+        Console.WriteLine("Escreva uma matrícula na forma AA-99-99:");
+        matricula = Console.ReadLine();
+
+        try
+        {
+            Regex re = new Regex(@"^[A-Z]{2}-[0-9]{2}-\d{2}$");
+
+            matriculaOk = re.IsMatch(matricula);
+
+            if (matriculaOk)
+                Console.WriteLine("Matrícula válida!");
+            else
+                Console.WriteLine("Matrícula inválida!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ocorreu um erro: " + ex.Message);
+        }
+    }
+}
