08dcd1e12c4b76d7c75577c1579bd70783c74742
mariaclr24 <160483532+mariaclr24@users.noreply.github.com>
2025-10-22 12:04:32 +0100

    ER com C#

--- ER/Program.cs
+++ ER/Program.cs
@@ -3,28 +3,30 @@ using System.Text.RegularExpressions;
 
 class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         string matricula = "";
-        bool matriculaOk = false;
+        bool matriculaok = false;
+
+        // Expressão regular para validar o formato AA-99-99
+        string erString = @"^[A-Z]{2}-[0-9]{2}-\d{2}$";
 
         Console.WriteLine("Escreva uma matrícula na forma AA-99-99:");
         matricula = Console.ReadLine();
 
         try
         {
-            Regex re = new Regex(@"^[A-Z]{2}-[0-9]{2}-\d{2}$");
+            Regex r = new Regex(erString);
+            matriculaok = r.IsMatch(matricula);
 
-            matriculaOk = re.IsMatch(matricula);
-
-            if (matriculaOk)
-                Console.WriteLine("Matrícula válida!");
+            if (matriculaok)
+                Console.WriteLine("Matrícula válida");
             else
-                Console.WriteLine("Matrícula inválida!");
+                Console.WriteLine("Matrícula inválida");
         }
         catch (Exception ex)
         {
-            Console.WriteLine("Ocorreu um erro: " + ex.Message);
+            Console.WriteLine("Erro: " + ex.Message);
         }
     }
 }
